dc4842c61952fcf6f0d97e2385847370b82a5340
Tomasz Chojnacki <tomaszchojnacki2001@gmail.com>
2022-11-06 17:57:53 +0100

    Finish third exercise

---
+++ Lista 5/Exercise3/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Exercise3.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Exercise3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(
+                new ErrorViewModel
+                {
+                    RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+                }
+            );
+        }
+    }
+}
