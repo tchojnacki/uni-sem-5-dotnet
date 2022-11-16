490583a78f79bfc79be4eeca185972bfce9a65bc
Tomasz Chojnacki <tomaszchojnacki2001@gmail.com>
2022-11-16 14:20:38 +0100

    Finish exercise 4

--- Lista 6/Exercise4/Program.cs
+++ Lista 6/Exercise4/Program.cs
@@ -6,7 +6,25 @@ namespace Exercise4
     {
         private static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var nameObject = new { FirstName = "Tomasz", LastName = "Chojnacki", Age = 21 };
+            MethodTakingAnonymousTypeByDynamic(nameObject);
+            MethodTakingAnonymousTypeWithReflection(nameObject);
+        }
+
+        private static void MethodTakingAnonymousTypeByDynamic(dynamic nameObj)
+        {
+            Console.WriteLine($"Name: {nameObj.FirstName} {nameObj.LastName}, age: {nameObj.Age}");
+        }
+
+        private static void MethodTakingAnonymousTypeWithReflection(object nameObj)
+        {
+            var type = nameObj.GetType();
+
+            var firstName = (string)type.GetProperty("FirstName")?.GetValue(nameObj);
+            var lastName = (string)type.GetProperty("LastName")?.GetValue(nameObj);
+            var age = (int?)type.GetProperty("Age")?.GetValue(nameObj);
+
+            Console.WriteLine($"Name: {firstName} {lastName}, age: {age}");
         }
     }
 }
