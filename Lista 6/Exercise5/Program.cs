8c0c7413a4e40c304b5d655f63207f0baafa9a80
Tomasz Chojnacki <tomaszchojnacki2001@gmail.com>
2022-11-16 10:52:19 +0100

    Setup solution structure

---
+++ Lista 6/Exercise5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercise5
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
