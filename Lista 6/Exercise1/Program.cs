006f81e28c2a714430d01d42eef447ef222b6730
Tomasz Chojnacki <tomaszchojnacki2001@gmail.com>
2022-11-17 13:27:18 +0100

    Fix exercise 1

--- Lista 6/Exercise1/Program.cs
+++ Lista 6/Exercise1/Program.cs
@@ -10,43 +10,57 @@ namespace Exercise1
     {
         private static void Main()
         {
-            var nameTuple = (FirstName: "Tomasz", LastName: "Chojnacki", Age: 21);
+            var nameTuple = (
+                FirstName: "Tomasz",
+                LastName: "Chojnacki",
+                Age: 21,
+                Salary: 2_800.50m
+            );
             MethodTakingATuple(nameTuple);
         }
 
         private static void MethodTakingATuple(
-            (string FirstName, string LastName, int Age) nameTuple
+            (string FirstName, string LastName, int Age, decimal Salary) nameTuple
         )
         {
             // 0) Wypisanie krotki z domyślnym formatowaniem
             Console.WriteLine(nameTuple);
 
             // 1) Przepisywanie krotki do osobno zadeklarowanych zmiennych
-            (string firstName1, string lastName1, int age1) = nameTuple;
-            Console.WriteLine($"First name: {firstName1}, last name: {lastName1}, age: {age1}");
+            (string firstName1, string lastName1, int age1, decimal salary1) = nameTuple;
+            Console.WriteLine(
+                $"Name: {firstName1} {lastName1}, age: {age1}, salary: {salary1} PLN"
+            );
 
             // 2) Przepisywanie krotki do osobno deklarowanych wcześniej zmiennych
             string firstName2;
             string lastName2;
             int age2;
-            (firstName2, lastName2, age2) = nameTuple;
-            Console.WriteLine($"Name: {firstName2} {lastName2}, age: {age2}");
+            decimal salary2;
+            (firstName2, lastName2, age2, salary2) = nameTuple;
+            Console.WriteLine(
+                $"Name: {firstName2} {lastName2}, age: {age2}, salary: {salary2} PLN"
+            );
 
-            // 3) Przepisywanie krotki do osobno deklarowanych zmiennych z niejawnie określonym typem
-            (var firstName3, var lastName3, var age3) = nameTuple;
-            Console.WriteLine($"Name: {firstName3} {lastName3}, age: {age3}");
+            // 3) Przepisywanie krotki do osobno deklarowanych zmiennych z niejawnie określonym typem, pominięcie elementu
+            (var firstName3, var lastName3, _, var salary3) = nameTuple;
+            Console.WriteLine($"Name: {firstName3} {lastName3}, salary: {salary3} PLN");
 
             // 4) Przepisywanie krotki do osobno deklarowanych zmiennych z niejawnie określonym typem wszystkich zmiennych
-            var (firstName4, lastName4, age4) = nameTuple;
-            Console.WriteLine($"Name: {firstName4} {lastName4}, age: {age4}");
+            var (firstName4, lastName4, age4, salary4) = nameTuple;
+            Console.WriteLine(
+                $"Name: {firstName4} {lastName4}, age: {age4}, salary: {salary4} PLN"
+            );
 
             // 5) Dostęp do elementów za pomocą nazw nadanych
             Console.WriteLine(
-                $"Name: {nameTuple.FirstName} {nameTuple.LastName}, age: {nameTuple.Age}"
+                $"Name: {nameTuple.FirstName} {nameTuple.LastName}, age: {nameTuple.Age}, salary: {nameTuple.Salary} PLN"
             );
 
             // 6) Dostęp do elementów za pomocą nazw domyślnych
-            Console.WriteLine($"Name: {nameTuple.Item1} {nameTuple.Item2}, age: {nameTuple.Item3}");
+            Console.WriteLine(
+                $"Name: {nameTuple.Item1} {nameTuple.Item2}, age: {nameTuple.Item3}, salary: {nameTuple.Item4} PLN"
+            );
         }
     }
 }
