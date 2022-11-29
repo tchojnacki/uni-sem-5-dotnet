4b6e52726bf40dead260887cb2783e58c13692aa
Tomasz Chojnacki <tomaszchojnacki2001@gmail.com>
2022-11-29 14:12:37 +0100

    Finish exercise 2

--- Lista 8/WebApp/Controllers/GameController.cs
+++ Lista 8/WebApp/Controllers/GameController.cs
@@ -5,27 +5,71 @@ namespace WebApp.Controllers
 {
     public class GameController : Controller
     {
-        public const int DefaultN = 30;
+        #region Logic
 
+        public const int DefaultN = 30;
         private static readonly Random Rand = new();
 
-        private static int N = DefaultN;
-        private static int RandValue = Rand.Next(N);
-        private static int GuessAttempts = 0;
+        private static int _n = DefaultN;
+        private static int _randValue = Rand.Next(_n);
+        private static int _guessAttempts;
+
+        private static void RerollNumber()
+        {
+            _randValue = Rand.Next(_n);
+            _guessAttempts = 0;
+        }
+
+        private static void UpdateRange(int n)
+        {
+            _n = n;
+            RerollNumber();
+        }
+
+        #endregion
+
+        #region Actions
 
         public IActionResult Set(int n)
         {
+            UpdateRange(n);
+
+            ViewBag.NewRange = n;
+
             return View();
         }
 
         public IActionResult Draw()
         {
+            RerollNumber();
+
+            ViewBag.Range = _n;
+
             return View();
         }
 
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Guess(int guess)
         {
+            _guessAttempts++;
+            ViewBag.GuessAttempts = _guessAttempts;
+
+            ViewBag.ResultText = guess switch
+            {
+                _ when guess < 0 || guess >= _n => "Podano liczbę spoza zakresu.",
+                _ when guess < _randValue => "Podano za małą liczbę.",
+                _ when guess > _randValue => "Podano za dużą liczbę.",
+                _ => "Poprawna odpowiedź!"
+            };
+
+            ViewBag.ResultClass = guess == _randValue ? "text-success" : "text-danger";
+
+            if (guess == _randValue)
+                RerollNumber();
+
             return View();
         }
+
+        #endregion
     }
 }
