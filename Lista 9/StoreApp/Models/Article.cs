063b3b1c034f288b214e8da58ec40ec29301e069
Tomasz Chojnacki <tomaszchojnacki2001@gmail.com>
2022-12-07 22:45:57 +0100

    Rebuild Create, Edit and Index pages

--- Lista 9/StoreApp/Models/Article.cs
+++ Lista 9/StoreApp/Models/Article.cs
@@ -19,18 +19,18 @@ namespace StoreApp.Models
         public int Id { get; set; }
 
         [Required]
-        [MinLength(3)]
-        [MaxLength(100)]
+        [MinLength(3, ErrorMessage = "The field {0} must not be shorter than {1}.")]
+        [MaxLength(100, ErrorMessage = "The field {0} must not be longer than {1}.")]
         public string Name { get; set; } = string.Empty;
 
         [Required]
         [DataType(DataType.Currency)]
-        [Range(0, double.PositiveInfinity)]
+        [Range(0.01, double.PositiveInfinity, ErrorMessage = "The field {0} must be positive.")]
         public decimal Price { get; set; }
 
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", NullDisplayText = "N/A")]
-        [Display(Name = "Expiration date")]
+        [Display(Name = "Expiration Date")]
         public DateTime? ExpirationDate { get; set; }
 
         [Required]
