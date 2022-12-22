c3b9e837c534081a7d28c8f7d693026e9c117f88
Tomasz Chojnacki <tomaszchojnacki2001@gmail.com>
2022-12-22 11:58:15 +0100

    Add items to cart with JS

--- Lista 10/StoreApp/wwwroot/js/cart.js
+++ Lista 10/StoreApp/wwwroot/js/cart.js
@@ -1,1 +1,16 @@
-﻿
+﻿const __RequestVerificationToken = document.querySelector('input[name="__RequestVerificationToken"]')?.value;
+const cartBadge = document.getElementById("dyn-cart-badge");
+[...document.getElementsByClassName("act-add-to-cart")].forEach(form => {
+    form.addEventListener("submit", event => {
+        event.preventDefault();
+        fetch(
+            form.action,
+            {
+                method: form.method,
+                body: new URLSearchParams({ __RequestVerificationToken })
+            }
+        ).then(() => {
+            cartBadge.innerText = parseInt(cartBadge.innerText) + 1;
+        });
+    });
+});
