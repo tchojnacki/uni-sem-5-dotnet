fcd6f9aacf281a84d1aa32faaab8ac2d111d36bf
Tomasz Chojnacki <tomaszchojnacki2001@gmail.com>
2022-12-22 14:21:54 +0100

    Refactor cart badge updating

--- Lista 10/StoreApp/wwwroot/js/cart.js
+++ Lista 10/StoreApp/wwwroot/js/cart.js
@@ -1,5 +1,18 @@
 ﻿const __RequestVerificationToken = document.querySelector('input[name="__RequestVerificationToken"]')?.value;
-const cartBadge = document.getElementById("dyn-cart-badge");
+
+const updateBadge = () => {
+    const badgeElem = document.getElementById("dyn-cart-badge");
+    const cartItemCount = document
+        .cookie
+        .split("; ")
+        .filter(c => c.startsWith("article"))
+        .map(c => parseInt(c.split("=")[1]))
+        .reduce((s, a) => s + a, 0);
+
+    badgeElem.innerText = cartItemCount;
+    badgeElem.dataset.text = cartItemCount;
+}
+
 [...document.getElementsByClassName("act-add-to-cart")].forEach(form => {
     form.addEventListener("submit", event => {
         event.preventDefault();
@@ -9,10 +22,6 @@ const cartBadge = document.getElementById("dyn-cart-badge");
                 method: form.method,
                 body: new URLSearchParams({ __RequestVerificationToken })
             }
-        ).then(() => {
-            const newItemCount = parseInt(cartBadge.innerText) + 1;
-            cartBadge.innerText = newItemCount;
-            cartBadge.dataset.text = newItemCount;
-        });
+        ).then(() => updateBadge());
     });
 });
